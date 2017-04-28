4e77e11a6c904c7326d10cd98c36e2132d4b1ba9
jioherry <jioherry@gmail.com>
2017-04-28 16:05:36 +0800

    (改 ActiveRecord Scope) 在首頁目前可以點選「列出進行中訊息 | 列出一週內進行中的訊息 | 列出所有已完成訊息」過濾出不同狀態和時間的 Messages。請修改 controllers/messages_controller.rb L10-11, L13-14, L18-19 和 models/message.rb 重構成使用 ActiveRecord Scope。

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,10 @@ class Message < ApplicationRecord
   belongs_to :user
 
   has_many :comments, :dependent => :destroy
+  scope :pending, -> { where status: 'pending' }
+  scope :completed, -> { where status: 'completed' }
+  scope :within_days , ->(time) { where "created_at >= ?", Time.now - time.to_i.days }
+  
 
   def last_comment_summary
     self.comments.last.try(:content).try(:truncate, 20)
