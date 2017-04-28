e5a94d2692df64f374a0bb6cc35f9b33e2fac96e
jioherry <jioherry@gmail.com>
2017-04-28 17:29:21 +0800

    (實作已經寫好，加單元測試) 在 models/user.rb 中，已經寫好一個 #words_count 實作，會回傳該使用者所有張貼的總字數。請修改 spec/models/user_spec.rb 加上 #words_count 的單元測試 (該實作不要改，只加單元測試)。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,11 +25,25 @@ RSpec.describe User, type: :model do
   describe "#words_count" do
     before do
       # TODO: 加 Message 和 Comment 測試資料
+      @other_user = User.create!(:email => "ooxx@example.org", :password => "12345678")
+      # 共60字 Message @user
+      m1 =Message.create!(:user => @user,
+                          :title => Faker::Lorem.sentence(10, false, 1),
+                          :content => Faker::Lorem.sentence(50, false, 1))
+      # 共20字 Comment @user
+      Comment.create!(:user => @user,
+                      :content => Faker::Lorem.sentence(20, false, 1),
+                      :message_id => m1)
+      # 共30字 Comment other_user
+      Comment.create!(:user => @other_user,
+                      :content => Faker::Lorem.sentence(30, false, 1),
+                      :message_id => m1)
     end
 
     it "加總該使用者的所有 Mesasge 和 Comment 的總字數" do
       # TODO: 測試 words_count 方法
-
+      expect(@user.words_count).to eq(80)
+      expect(@other_user.words_count).to eq(30)
     end
   end
 
